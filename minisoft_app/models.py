c01fcd89050cb6b356d3c5d061a1e1326be1bcf8
Toqui012 <alvaroseba2000@gmail.com>
2020-07-04 04:15:34 -0400

    Incorporación base de datos y listado

--- minisoft_app/models.py
+++ minisoft_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+load_family = [
+    ('Hogar','Hogar'),
+    ('Viaje','Viaje'),
+    ('Jugueteria','Jugueteria'),
+    ('Salud y Belleza','Salud y Belleza'),
+    ('Deportes','Deportes'),
+    ('Licencia','Licencia'),
+]
+
+
+class Producto(models.Model):
+
+    num_cuenta = models.BigIntegerField(null=False)
+    familia = models.CharField(max_length=255, null=False, choices=load_family, default='Hogar')
+    color = models.CharField(max_length=255, null=False)
+    descripcion = models.CharField(max_length=255, null=False)
+    cantidad = models.BigIntegerField(null=False)
+    merma = models.BooleanField(null=False)
+    precio = models.BigIntegerField(null=False)
+
+    def __str__(self):
+        return str(self.pk)
+
--- minisoft_app/views.py
+++ minisoft_app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.template.context_processors import request
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import Producto
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+    list = Producto.objects.all()
+    return render(request,'index.html',{'list':list})
 
 def login_view(request):
     return render(request,'login_view.html')
